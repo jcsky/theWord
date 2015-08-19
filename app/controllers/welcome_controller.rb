77280328bb2b778dbbdeb4766c7f87e1a0a7e090
王信凱 <jcskyting@gmail.com>
2015-08-19 14:49:13 +0800

    slider partial

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,26 @@
 class WelcomeController < ApplicationController
   def index
-
+    @btitle = "最後一天別讓家人煩腦"
+    @stitle = "留下你的器捐意願、安寧意願、喪葬意願"
+    @search_text = "輸入Email查詢親友的再見手札"
   end
 
   def organ
-
+    @btitle = "最後一天別讓家人煩腦"
+    @stitle = "留下你的器捐意願"
+    @search_text = "輸入Email查詢親友的器捐意願"
   end
 
   def hospice
-
+    @btitle = "最後一天別讓家人煩腦"
+    @stitle = "留下你的安寧意願"
+    @search_text = "輸入Email查詢親友的安寧意願"
   end
 
   def funeral
-
+    @btitle = "最後一天別讓家人煩腦"
+    @stitle = "留下你的喪葬意願"
+    @search_text = "輸入Email查詢親友的喪葬意願"
   end
 
 end
