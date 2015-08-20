34cbb05715cfd1ab12c909166800f658fd8dfa43
王信凱 <jcskyting@gmail.com>
2015-08-20 11:34:35 +0800

    change controller user to users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+  end
+
+  def update
+    if current_user.update(set_user)
+      if params[:from] == "signup"
+        redirect_to new_contact_path
+      else
+        redirect_to messages_path
+      end
+    else
+      redirect_to :back
+    end
+  end
+
+  def set_user
+    params.require(:user).permit(:name, :email, :address, :phone, :home_phone)
+  end
+
+end
