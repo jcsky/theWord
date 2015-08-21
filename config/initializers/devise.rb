d712907143c063f4971a2f938b2589f26fef44de
王信凱 <jcskyting@gmail.com>
2015-08-21 11:50:38 +0800

    fb login done

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -121,7 +121,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
@@ -241,7 +241,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   fb_config = YAML.load(File.read("#{Rails.root}/config/oauth.yml"))[Rails.env]
 
-  config.omniauth :facebook, fb_config['fb_id'] , fb_config['fb_secret'], :scope => 'email, user_photos'
+  config.omniauth :facebook, fb_config['fb_id'] , fb_config['fb_secret'], scope: 'email', info_fields: 'email, name'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821030912) do
+ActiveRecord::Schema.define(version: 20150821034553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20150821030912) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "image"
+    t.string   "phone"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
