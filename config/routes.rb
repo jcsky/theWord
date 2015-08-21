cffb64a767b4266e1f9b52da993e8902e027e28c
王信凱 <jcskyting@gmail.com>
2015-08-21 14:16:29 +0800

    init user show page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+
+  resources :users
+
   get 'hospice' => 'welcome#hospice'
   get 'organ-donation' => 'welcome#organ'
   get 'funeral' => 'welcome#funeral'
