55413e0fdf9ae184fc8b5e6cad06f3fa03f6dcdd
王信凱 <jcskyting@gmail.com>
2015-09-16 09:57:02 +0800

    bundle

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,13 @@ class User < ActiveRecord::Base
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
       if session[:byenote_params]
         user.byenote = Byenote.new(session[:byenote_params])
       end
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+
     end
   end
 
